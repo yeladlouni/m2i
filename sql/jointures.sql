806b13951f6e30ea4fed4d54486742317837d208
= <=>
2020-11-02 15:11:26 +0100

    dummy

--- sql/jointures.sql
+++ sql/jointures.sql
@@ -1,75 +1,75 @@
--- Sélectionner les employés avec le nom du département auquel ils sont affectés
+-- Sï¿½lectionner les employï¿½s avec le nom du dï¿½partement auquel ils sont affectï¿½s
 
--- 1ère syntaxe (ambiguïté en cas de présence de deux colonnes en commun)
+-- 1ï¿½re syntaxe (ambiguï¿½tï¿½ en cas de prï¿½sence de deux colonnes en commun)
 
 SELECT employee_id, first_name, last_name, department_name
 FROM employees
 NATURAL JOIN departments;
 
--- 2ème syntaxe
+-- 2ï¿½me syntaxe
 
 SELECT employee_id, first_name, last_name, department_name
 FROM employees
 JOIN departments USING (department_id);
 
--- 3ème syntaxe
+-- 3ï¿½me syntaxe
 
 SELECT employee_id, first_name, last_name, department_name
 FROM employees
 JOIN departments  ON employees.department_id = departments.department_id;
 
 
--- Sélectionner les employés même ceux qui ne sont affectés à aucun département
+-- Sï¿½lectionner les employï¿½s mï¿½me ceux qui ne sont affectï¿½s ï¿½ aucun dï¿½partement
 
 SELECT employee_id, first_name, last_name, emp.department_id, dpt.department_name
 FROM employees emp
 LEFT OUTER JOIN departments dpt ON emp.department_id = dpt.department_id;
 
--- Sélectionner les employés affectés aux différents départrments et afficher même les départements auquels aucun employé n'est affecté
+-- Sï¿½lectionner les employï¿½s affectï¿½s aux diffï¿½rents dï¿½partrments et afficher mï¿½me les dï¿½partements auquels aucun employï¿½ n'est affectï¿½
 
 SELECT employee_id, first_name, last_name, department_name
 FROM employees emp
 RIGHT OUTER JOIN departments dpt ON emp.department_id = dpt.department_id
 
 
--- Sélectionner les employés affectés aux différents départements ainsi que les employés qui ne sont affectés à aucun des département
--- et les département auquels aucun employé n'est affecté
+-- Sï¿½lectionner les employï¿½s affectï¿½s aux diffï¿½rents dï¿½partements ainsi que les employï¿½s qui ne sont affectï¿½s ï¿½ aucun des dï¿½partement
+-- et les dï¿½partement auquels aucun employï¿½ n'est affectï¿½
 
 SELECT employee_id, first_name, last_name, department_name
 FROM employees emp
 FULL OUTER JOIN departments dpt ON emp.department_id = dpt.department_id
 
 
--- Exemple 1: Afficher pour les employés le ID, le prénom, le nom, le salaire et le nom du département auquel il est affecté
+-- Exemple 1: Afficher pour les employï¿½s le ID, le prï¿½nom, le nom, le salaire et le nom du dï¿½partement auquel il est affectï¿½
 
 SELECT employee_id, first_name, last_name, salary, department_name
 FROM employees emp
 LEFT OUTER JOIN DEPARTMENTS dpt ON (emp.department_id = dpt.department_id);
 
--- Exemple 2: Afficher pour les employés l'ID, le prénom, le nom, le salaire et le nom du département auquel il est affecté
--- Pour les employés qui ne sont affectés à aucun département, afficher "Non affecté" comme nom de département
+-- Exemple 2: Afficher pour les employï¿½s l'ID, le prï¿½nom, le nom, le salaire et le nom du dï¿½partement auquel il est affectï¿½
+-- Pour les employï¿½s qui ne sont affectï¿½s ï¿½ aucun dï¿½partement, afficher "Non affectï¿½" comme nom de dï¿½partement
 
 
-SELECT employee_id, first_name, last_name, salary, NVL(department_name,'Non affecté')
+SELECT employee_id, first_name, last_name, salary, NVL(department_name,'Non affectï¿½')
 FROM employees emp
 LEFT OUTER JOIN DEPARTMENTS dpt ON (emp.department_id = dpt.department_id);
 
--- Ecrire une requête afin d'afficher pour chaque département, le nom de département et le nombre d'employés affectés
+-- Ecrire une requï¿½te afin d'afficher pour chaque dï¿½partement, le nom de dï¿½partement et le nombre d'employï¿½s affectï¿½s
 SELECT department_name, COUNT(employee_id)
 FROM employees
 JOIN departments USING (department_id)
 GROUP BY department_name;
 
 
--- Ecrire une requête afin d'afficher pour chaque département, le nom de département et le nombre d'employés affectés
--- Afficher un nombre d'employés à 0 pour les départements auquels aucun employé n'est affecté
+-- Ecrire une requï¿½te afin d'afficher pour chaque dï¿½partement, le nom de dï¿½partement et le nombre d'employï¿½s affectï¿½s
+-- Afficher un nombre d'employï¿½s ï¿½ 0 pour les dï¿½partements auquels aucun employï¿½ n'est affectï¿½
 
 SELECT NVL(department_name, 'Autres'), COUNT(employee_id)
 FROM employees emp
 LEFT OUTER JOIN departments dpt ON (emp.department_id = dpt.department_id)
 GROUP BY department_name;
 
--- Ecrire une requête pour afficher le nom des département auquels sont affectés plus que 10 employés
+-- Ecrire une requï¿½te pour afficher le nom des dï¿½partement auquels sont affectï¿½s plus que 10 employï¿½s
 SELECT NVL(department_name, 'Autres'), COUNT(employee_id)
 FROM employees emp
 LEFT OUTER JOIN departments dpt ON (emp.department_id = dpt.department_id)
@@ -79,9 +79,7 @@ ORDER BY COUNT(employee_id);
 
 
 
--- Ecrire une requête pour afficher le total des salaires par département, afficher 0 pour les départements auquels aucun 
--- employé n'est affecté
+-- Ecrire une requï¿½te pour afficher le total des salaires par dï¿½partement, afficher 0 pour les dï¿½partements auquels aucun 
+-- employï¿½ n'est affectï¿½
 
 
-
--- Rappel: Le total des salaires est de 691416
