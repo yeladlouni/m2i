5abde5e428a1dc49269799c24c44ab0071ecc500
= <=>
2020-11-02 16:59:39 +0100

    projets

--- sql/jointures.sql
+++ sql/jointures.sql
@@ -90,3 +90,41 @@ WHERE emp1.employee_id = emp2.manager_id
 
 -- Ecrire une requête afin d'afficher pour chaque nom de région, le nombre total des employés
 -- Afficher 0 pour les régions sans employés
+
+SELECT NVL(region_name, 'autre'), COUNT(employee_id)
+From employees emp
+LEFT JOIN departments dpt ON dpt.department_id = emp.department_id
+LEFT JOIN locations loc ON loc.location_id = dpt.location_id
+LEFT JOIN countries co ON co.country_id = loc.country_id
+LEFT JOIN regions reg ON reg.region_id = co.region_id
+GROUP BY region_name;
+
+-- Ecrire une requête afin d'afficher pour chaque département et chaque fonction le nombre d'employés, 0 pour les combinaisons
+-- qui ne correspondent à aucun employé
+
+SELECT department_name, job_title, COUNT(employee_id)
+FROM
+(SELECT department_id, job_id
+FROM departments
+CROSS JOIN jobs) cmb
+FULL OUTER JOIN employees emp ON (emp.department_id = cmb.department_id AND emp.job_id = cmb.job_id)
+FULL OUTER JOIN departments dpt ON (cmb.department_id = dpt.department_id)
+FULL OUTER JOIN jobs job ON (cmb.job_id = job.job_id)
+GROUP BY department_name, job_title;
+
+
+-- Ecrire une requête (en utilisant une requête imbriquée/sous requête) afin de retourner tous les employés dont la fonction est Programmer
+SELECT last_name, job_id
+FROM employees
+WHERE job_id = (SELECT job_id
+            FROM jobs
+            WHERE job_title LIKE 'Programmer')
+
+-- Ecrire une requête (en utilisant une requête imbriquée/sous requête) afin de retourner tous les employés dont la fonction commence avec un "P"
+SELECT last_name, job_id
+FROM employees
+WHERE job_id IN (SELECT job_id
+            FROM jobs
+            WHERE job_title LIKE ('P%'))
+            
+            
