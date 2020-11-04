b9ed1328bc394b889cca63efd33b1218bae990bd
= <=>
2020-11-04 17:34:35 +0100

    practice3

--- sql/security.sql
+++ sql/security.sql
@@ -28,4 +28,23 @@ GRANT CREATE TABLE TO yassine2;
 
 GRANT SELECT, UPDATE, DELETE ON hr.employees TO yassine2;
 
--- Créer un ro
+-- Créer un rôle manager avec le droit de consulter, mettre à jour et supprimer les données de la table products
+
+CREATE ROLE manager;  -- Créer le rôle manager
+
+GRANT SELECT, UPDATE, DELETE ON shopping.products to manager; -- Accorder les droits de consultation, modification et suppression sur toutes les tables
+
+GRANT manager TO yassine2; -- Accorde le rôle manager à l'utilisateur yassine2
+ 
+CREATE ROLE marketing; -- Création du rôle marketing
+
+GRANT SELECT ON products TO marketing; -- Accorder le droit de consultation de la table products au rôle marketing
+
+CREATE USER dupont IDENTIFIED BY yassine_2020; -- Créer l'utilisateur dupont avec le mot de passe yassine_2020
+
+GRANT marketing TO dupont; -- Accorder le rôle marketing à l'utilisateur dupont
+
+
+GRANT UNLIMITED TABLESPACE TO yassine2; -- Donner le droit de création de données à yassine, Quota illimitée (espace disque)
+
+ALTER USER yassine2 QUOTA 100M ON USERS; -- Spécifier un quota pour un utilisateur sur le tablespace USERS
