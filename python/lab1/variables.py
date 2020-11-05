f6d1a35572342e2d730fda1d9b80b8759c2fbb64
= <=>
2020-11-05 12:05:56 +0100

    strings

--- python/lab1/variables.py
+++ python/lab1/variables.py
@@ -1,3 +1,20 @@
+
+# Variables strings
+
 message = "Hello Hello!"
 
 print(message)
+
+# Changer la casse
+
+first_name = "dupont"
+last_name = "lewis"
+
+# La fonction title permet de convertir les initiaux d'une chaîne de caractères en majuscules
+
+print(first_name.title())
+
+message = "hello world!"
+
+print(message.title())
+
