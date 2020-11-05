c9973ec8126dcd574c13f9bace12c760cb90b18d
= <=>
2020-11-05 12:20:25 +0100

    f-strings

--- python/lab1/variables.py
+++ python/lab1/variables.py
@@ -18,3 +18,22 @@ message = "hello world!"
 
 print(message.title())
 
+# Afficher le first_name en majuscules
+
+print(first_name.upper())
+
+# Afficher le first_name en minuscules
+
+print(first_name.lower())
+
+# Afficher le first_name concaténé avec last_name
+
+print(first_name + " " + last_name)
+
+# On peut aussi utiliser ce qu'on appelle les f-strings, le f dénote format
+
+print(f"Le nom complet est: {first_name} {last_name}.")
+
+# Ecrire une commande python afin d'afficher le first_name et le last_name avec les initiaux en majuscules
+
+
