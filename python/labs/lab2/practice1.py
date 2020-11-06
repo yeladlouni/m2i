43ee4ccabc516cda6d397a4737f5236b54a12baa
= <=>
2020-11-06 11:21:58 +0100

    practice2

--- python/labs/lab2/practice1.py
+++ python/labs/lab2/practice1.py
@@ -7,4 +7,4 @@
 # Afficher le nombre de caractères de chaque pays
 # Afficher la liste des pays comme chaîne de caractères séparés par une virgule et un espace
 # Afficher les 3 derniers caractère de chaque pays en majuscules
-
+# Convertir la liste en tuple
