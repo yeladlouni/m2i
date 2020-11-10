244e510bcfc05300714a7ba3df57ecae97133c82
= <=>
2020-11-10 17:31:38 +0100

    lab7, files

--- python/labs/lab7/practice.py
+++ python/labs/lab7/practice.py
@@ -6,3 +6,11 @@ with open('../../../data/hr/departments.csv') as f:
 with open('../../../data/hr/employees.csv') as f:
     employees = f.readlines()
 
+import json
+
+with open('employees.json', 'w') as f:
+    json.dump({'id': 10, 'first_name': 'king'}, f)
+
+with open('employees.json') as f:
+    employees = json.load(f)
+    print(employees['id'])
