a843f55e47f2ef37b96193bfa4bcaf6dcb5cc5b7
= <=>
2020-11-20 16:57:49 +0100

    scrapy

--- scraping/labs/lab6/yellowpages.py
+++ scraping/labs/lab6/yellowpages.py
@@ -3,7 +3,7 @@ import scrapy
 
 class YellowPagesSpider(scrapy.Spider):
     name = 'yellowpages'
-    start_urls = ['https://www.pagesjaunes.fr/annuaire/chercherlespros?quoiqui=pharmacie&ou=Paris']
+    start_urls = ['https://www.pagesjaunes.fr/annuaire/chercherlespros?quoiqui=pharmacie&ou=' + city for city in ['Paris', 'Toulouse', 'Lille', 'Lyon']]
 
     def parse(self, response):
         companies = response.css(' div.zone-bi > header ')
@@ -11,5 +11,5 @@ class YellowPagesSpider(scrapy.Spider):
             company_name = company.css('div.row-denom > div.denomination.with-stars > h3 > a ::text').get()
             company_address = company.css('div.main-adresse-container.row-adresse.with-adresse.with-horaire-chaudes > div.adresse-container.noTrad > a ::text').get
 
-            yield {'name': company_name.replace_all('"', '')}
+            yield {'name': company_name}
 
