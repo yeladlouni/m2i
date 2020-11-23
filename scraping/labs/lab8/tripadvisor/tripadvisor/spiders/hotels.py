77950ea4a8910987a5a55f7a8968abbc5945ee29
= <=>
2020-11-23 13:53:27 +0100

    lab 8 solution

--- scraping/labs/lab8/tripadvisor/tripadvisor/spiders/hotels.py
+++ scraping/labs/lab8/tripadvisor/tripadvisor/spiders/hotels.py
@@ -1,25 +1,29 @@
-from scrapy.spiders import CrawlSpider
+import scrapy
 from scrapy import Request
+from ..items import HotelItem
 
-class HotelsSpider(CrawlSpider):
+
+class HotelsSpider(scrapy.Spider):
     name = 'hotels'
     allowed_domains = ['tripadvisor.fr']
     start_urls = ['https://www.tripadvisor.fr/Hotels-g45963-Las_Vegas_Nevada-Hotels.html']
 
-    def parse_items(self, response):
+    def parse(self, response):
         hotels = response.css('div.listItem')
         for item in hotels:
             hotel = HotelItem()
 
-            hotel['name'] = item.css('div.listing_title a ::text').get()
-            hotel['review'] = item.css('a.ui_bubble_rating ::attr(alt)').get()
-            hotel['reviewCount'] = item.css('a.review_count ::text').get()
+            hotel['name'] = item.css('div.listing_title a ::text').get().strip()
+            hotel['review'] = float(item.css('a.ui_bubble_rating ::attr(alt)').get().split()[0].replace(',', '.')) if ' ' in item.css('a.ui_bubble_rating ::attr(alt)').get() else 0
+            hotel['reviewCount'] = item.css('a.review_count ::text').get().replace(' avis', '')
 
-            next_page = response.css('a.next ::attr(href)').get()
+            yield hotel
 
-            if next_page:
-                print(response)
-                next_page = response.urljoin(next_page)
-                yield Request(url=next_page, callback=self.parse)
+        next_page = response.css('a.next ::attr(href)').get()
+        print(next_page)
+        if next_page:
+            print(response)
+            next_page = response.urljoin(next_page)
+            yield Request(url=next_page, callback=self.parse)
 
-            return hotel
+
