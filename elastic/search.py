dfbdcf04452615c155ee0748391856d3eccc44fd
= <=>
2020-12-14 09:38:36 +0100

    notebooks elk

--- elastic/search.py
+++ elastic/search.py
@@ -7,10 +7,11 @@ from elasticsearch_dsl import Search, Q
 # Création d'un handle vers le cluster ElasticSearch
 
 es = Elasticsearch(
-    'local'
+    'localhost'
 )
 
 # Pour renvoyer le résultat sous forme d'un JSON = Lightweight search
+es.index()
 
 res = es.search(index='hr', doc_type='employee')
 
@@ -24,3 +25,13 @@ s.execute()
 
 for hit in s:
     print(hit.first_name, hit.last_name, hit.age)
+
+doc = {
+    'id': 1,
+    'first_name': 'test',
+    'last_name': 'test',
+    'age': 30,
+    'interests': ['sport', 'hiking', 'biking']
+}
+
+es.index(index='hr', doc_type='employee', body=doc)
